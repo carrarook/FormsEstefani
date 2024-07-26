ed1a35dcc050595d50503a7e33d3d5e79dd59070
carrarook <70044423+carrarook@users.noreply.github.com>
2024-07-26 20:49:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function displayResults() {
         popup.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
         popup.style.textAlign = "center";
         popup.innerHTML = `<p>Parabéns! Você acertou todas as perguntas! E ganhou o seguinte vale:</p>
-                           <img src="mnt/data/image.png" alt="Premio" style="max-width: 100%; height: auto;">
+                           <img src="Voucher.png" alt="Premio" style="max-width: 100%; height: auto;">
                            <button onclick="document.body.removeChild(this.parentElement)">Fechar</button>`;
         document.body.appendChild(popup);
     } else {
